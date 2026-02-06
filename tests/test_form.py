4700638b6bfae2c5087cfc0328094df8c917d430
Shriyadwivedi1234 <shriyadwivedi29@gmail.com>
2026-02-06 20:58:58 +0530

    fixed test_form wait issue

--- tests/test_form.py
+++ tests/test_form.py
@@ -2,13 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from utils.screenshots import take_screenshot
+
 
 
 def login(driver, wait):
 
     driver.get("https://opensource-demo.orangehrmlive.com")
 
-    wait.until(EC.presence_of_element_located((By.NAME,"username"))).send_keys("Admin")
+    wait.until(EC.visibility_of_element_located((By.NAME,"username"))).send_keys("Admin")
     driver.find_element(By.NAME,"password").send_keys("admin123")
     driver.find_element(By.XPATH,"//button[@type='submit']").click()
 
@@ -24,20 +26,38 @@ def test_add_employee():
     wait.until(EC.element_to_be_clickable((By.XPATH,"//span[text()='PIM']"))).click()
 
     # Add Employee
-    wait.until(EC.element_to_be_clickable((By.XPATH,"//a[text()='Add Employee']"))).click()
+    add_btn = wait.until(
+    EC.element_to_be_clickable((By.XPATH, "//a[text()='Add Employee']"))
+    )
+    add_btn.click()
+
+    wait.until(EC.visibility_of_element_located((By.NAME, "firstName")))
 
     # Fill Form
-    driver.find_element(By.NAME,"firstName").send_keys("Test")
-    driver.find_element(By.NAME,"lastName").send_keys("User")
+    first_name = wait.until(
+        EC.visibility_of_element_located((By.NAME, "firstName"))
+    )
+    first_name.send_keys("Test")
+
+    last_name = wait.until(
+        EC.visibility_of_element_located((By.NAME, "lastName"))
+    )
+    last_name.send_keys("User")
+
 
     # Save
     driver.find_element(By.XPATH,"//button[@type='submit']").click()
 
     # Verify
     success = wait.until(
-        EC.presence_of_element_located((By.XPATH,"//h6[text()='Personal Details']"))
+        EC.visibility_of_element_located((By.XPATH, "//h6[text()='Personal Details']"))
     )
 
-    assert success.is_displayed()
+    try:
+        assert success.is_displayed()
+
+    except:
+        take_screenshot(driver, "add_employee_fail")
+        raise
 
     driver.quit()
